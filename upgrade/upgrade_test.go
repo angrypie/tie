7271ef78d3f1e697d0d9956a84f33d1237cd77f8
angrypie <cookies.in.chocolate@gmail.com>
2017-07-08 22:20:49 +0300

    Upgrade test now scan example/base package

--- upgrade/upgrade_test.go
+++ upgrade/upgrade_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestUpgrade(t *testing.T) {
-	upgrade, err := Server("github.com/angrypie/tie/upgrade")
+	upgrade, err := Server("github.com/angrypie/tie/example/basic")
 	if err != nil {
 		t.Error(err)
 	}
