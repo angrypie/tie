ff53eeb595eda1505963800273e416d0f24c0135
angrypie <cookies.in.chocolate@gmail.com>
2018-08-28 12:02:58 +0300

    Fix lint suggestion

--- example/custom-types/cli/cli.go
+++ example/custom-types/cli/cli.go
@@ -3,6 +3,6 @@ package main
 import "github.com/angrypie/tie/example/custom-types/register"
 
 func main() {
-	user := register.User{"Paul", "PaulPassword"}
+	user := register.User{Name: "Paul", Password: "PaulPassword"}
 	register.NewUser(user)
 }
